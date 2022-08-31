a2f02b008beb62816339ef0ad67370342fafacf1
Dan Vyenielo <dvyenielo@protonmail.com>
2022-08-31 12:38:38 -0700

    Add commentary on explicit solution.

--- p1_multiples_of_3_or_5/solution.cpp
+++ p1_multiples_of_3_or_5/solution.cpp
@@ -1,4 +1,10 @@
 #include <bits/stdc++.h>
+// note we can compute the value directly by
+// 3 * sum(1,1000/3, 1) + 5 * sum(1, 1000/5 - 1, 1) - 15 * sum(1, 1000/15, 1)
+// where the divisions are floored and sum(...) can be substituted with the 
+// sum of a consecutive sequence between 1 and n (1, 2, 3, ... , n) with n*(n+1)/2
+// the formula is then
+// 3(333*334/2) + 5(199*200/2) - 15(66*67/2) = 233168
 int main()
 {
     long sum{ 0 };
