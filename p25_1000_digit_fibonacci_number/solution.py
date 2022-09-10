197347d9da362073a3f65923a6c3e48d4346204f
Dan Vyenielo <dvyenielo@protonmail.com>
2022-09-09 21:01:06 -0700

    As accepted

---
+++ p25_1000_digit_fibonacci_number/solution.py
@@ -0,0 +1,19 @@
+def digit_count(n):
+    ret = 0  # digit count
+    while n > 0:
+        ret += 1
+        n //= 10
+    return ret
+
+
+pre = 1 # F_1
+cur = 1 # F_2
+i = 2
+dc = 1
+while dc < 1000:
+    nxt = pre + cur
+    dc = digit_count(nxt)
+    pre = cur
+    cur = nxt
+    i += 1
+print(str(i))
